91605aadd6e8f6bbc90a4c5c6f02e67ef57ac35e
AfiNaufal97 <naufal.setiawan92@gmail.com>
2020-12-08 13:54:25 +0700

    Adapter Activity Mobil

--- oop2/app/src/main/java/id/ac/politeknikharber/oop2/mobilfile/MobilAdapter.kt
+++ oop2/app/src/main/java/id/ac/politeknikharber/oop2/mobilfile/MobilAdapter.kt
@@ -1,2 +1,31 @@
 package id.ac.politeknikharber.oop2.mobilfile
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import id.ac.politeknikharber.oop2.R
+import id.ac.politeknikharber.oop2.database.mobil.Mobil
+import kotlinx.android.synthetic.main.item_mobil.view.*
+
+class MobilAdapter(private val dataMobil:ArrayList<Mobil>) :RecyclerView.Adapter<MobilAdapter.MylistMobil>(){
+    class MylistMobil(itemview: View):RecyclerView.ViewHolder(itemview)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MobilAdapter.MylistMobil {
+        return MylistMobil(LayoutInflater.from(parent.context).inflate(R.layout.item_mobil, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MobilAdapter.MylistMobil, position: Int) {
+        val isiDataMobil = dataMobil[position]
+        holder.itemView.tv_item_mobil.text = isiDataMobil.nama_mobil
+    }
+
+    override fun getItemCount() = dataMobil.size
+
+    fun setDataMobil(listMobil:List<Mobil>){
+        dataMobil.clear()
+        dataMobil.addAll(listMobil)
+        notifyDataSetChanged()
+    }
+
+}
