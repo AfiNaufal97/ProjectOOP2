29e7793ac820d7af6821119ed99aba469a3fd1c0
AfiNaufal97 <naufal.setiawan92@gmail.com>
2020-12-08 13:38:08 +0700

    update table mobil

--- oop2/app/src/main/java/id/ac/politeknikharber/oop2/database/mobil/Mobil.kt
+++ oop2/app/src/main/java/id/ac/politeknikharber/oop2/database/mobil/Mobil.kt
@@ -6,13 +6,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
-@Entity(tableName = "mobil")
-
-@Parcelize
-data class Mobil (
-    @PrimaryKey (autoGenerate = true) @ColumnInfo(name="id") var id:Int=0,
-    @ColumnInfo(name="no_kendaraan") var no_kendaraan:String="",
-    @ColumnInfo(name = "jenis_mobil") var jenis_mobil:String="",
-    @ColumnInfo(name = "nama") var nama:String="",
-    @ColumnInfo(name="tarif") var tarif:String=""
-) : Parcelable
+@Entity
+data class Mobil(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int=0,
+    var nama_mobil:String="",
+    var jenis_mobil:String="",
+    var kapasitas:Int=0,
+    var no_polisi:String=""
+)
