b134e180c8cbe7fd82f4f48ea051b01d3e980f51
AfiNaufal97 <naufal.setiawan92@gmail.com>
2020-12-14 18:08:43 +0700

    activity pelanggan

--- oop2/app/src/main/java/id/ac/politeknikharber/oop2/filepengguna/PelangganActivity.kt
+++ oop2/app/src/main/java/id/ac/politeknikharber/oop2/filepengguna/PelangganActivity.kt
@@ -1,11 +1,22 @@
 package id.ac.politeknikharber.oop2.filepengguna
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import id.ac.politeknikharber.oop2.R
+import id.ac.politeknikharber.oop2.database.RentalDatabase
+import kotlinx.android.synthetic.main.activity_pelanggan.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class PelangganActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pelanggan)
+
     }
 }
