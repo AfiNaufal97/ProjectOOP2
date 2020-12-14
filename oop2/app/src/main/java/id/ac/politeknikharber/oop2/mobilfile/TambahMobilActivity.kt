9130c80335bd740e6f1a54af901079e50b09f832
AfiNaufal97 <naufal.setiawan92@gmail.com>
2020-12-14 07:24:30 +0700

    tambah activity tambah data

---
+++ oop2/app/src/main/java/id/ac/politeknikharber/oop2/mobilfile/TambahMobilActivity.kt
@@ -0,0 +1,13 @@
+package id.ac.politeknikharber.oop2.mobilfile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.politeknikharber.oop2.R
+
+class TambahMobilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_mobil)
+
+    }
+}
