baf31c4f7aff8f84845d06ce5d6fb5c5ce2111e3
AfiNaufal97 <naufal.setiawan92@gmail.com>
2020-12-14 18:04:52 +0700

    layout activity pelanggan

---
+++ oop2/app/src/main/java/id/ac/politeknikharber/oop2/filepengguna/TambahPalangganActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.politeknikharber.oop2.filepengguna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahPalangganActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_palanggan)
+    }
+}
