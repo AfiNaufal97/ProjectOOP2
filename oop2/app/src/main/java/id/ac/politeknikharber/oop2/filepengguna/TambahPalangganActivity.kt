e49ea0e63b7ca5928cd417d602762a9ede97cfe2
AfiNaufal97 <naufal.setiawan92@gmail.com>
2020-12-14 18:09:19 +0700

    activity tambah pelanggan

--- oop2/app/src/main/java/id/ac/politeknikharber/oop2/filepengguna/TambahPalangganActivity.kt
+++ oop2/app/src/main/java/id/ac/politeknikharber/oop2/filepengguna/TambahPalangganActivity.kt
@@ -1,9 +1,18 @@
 package id.ac.politeknikharber.oop2.filepengguna
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ac.politeknikharber.oop2.R
+import id.ac.politeknikharber.oop2.database.RentalDatabase
+import id.ac.politeknikharber.oop2.database.pelanggan.Pelanggan
+import kotlinx.android.synthetic.main.activity_tambah_palanggan.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class TambahPalangganActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tambah_palanggan)
