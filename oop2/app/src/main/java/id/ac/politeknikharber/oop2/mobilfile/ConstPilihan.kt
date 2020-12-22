af0142b6fb7bb13421d18c493046c5c96904bd68
AfiNaufal97 <naufal.setiawan92@gmail.com>
2020-12-22 14:50:07 +0700

    tambah class helper

--- oop2/app/src/main/java/id/ac/politeknikharber/oop2/mobilfile/ConstPilihan.kt
+++ oop2/app/src/main/java/id/ac/politeknikharber/oop2/mobilfile/ConstPilihan.kt
@@ -1,4 +1,9 @@
 package id.ac.politeknikharber.oop2.mobilfile
 
 class ConstPilihan {
+    companion object{
+        val READ_DATA = 1
+        val CREATE_DATA = 2
+        val UPDATE_DATA = 3
+    }
 }
